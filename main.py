2dd57518f4fede6d0ad002b3a5800aef6bd19ac6
KshitiShukla <kshitivshukla@gmail.com>
2025-12-18 14:47:27 +0530

    Creating File for Streamlit UI

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from scrape import scrapeWebsite
 
 st.title("AI Web Scrapper")
 url = st.text_input("Enter Website URL:")
 
 if st.button("Scrape Site"):
     st.write("Scrapping Website")
+    result = scrapeWebsite(url)
+    print(result)
