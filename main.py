290400ec8aa21bd561bd8426f113b91147e6d2f7
KshitiShukla <kshitivshukla@gmail.com>
2025-12-18 17:40:29 +0530

    Creating File for Streamlit UI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from scrape import scrapeWebsite
+from bs4 import BeautifulSoup
 
 st.title("AI Web Scrapper")
 url = st.text_input("Enter Website URL:")
@@ -8,3 +9,10 @@ if st.button("Scrape Site"):
     st.write("Scrapping Website")
     result = scrapeWebsite(url)
     print(result)
+
+def extractBodyContent(htmlContent):
+    soup = BeautifulSoup(htmlContent, "html.parser")
+    bodyContent = soup.body
+    if bodyContent:
+        return str(bodyContent)
+    return ""
